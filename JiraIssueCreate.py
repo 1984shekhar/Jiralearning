bdc416f630846e0e5a642a0a25aa2f8dafc29505
cs pandey <chandrashekharpandey@hotmail.com>
2022-01-09 18:04:49 +0530

    Jira Create Issue

--- JiraIssueCreate.py
+++ JiraIssueCreate.py
@@ -8,17 +8,18 @@ with open(payload, "r") as jsonfile:
  json_object = json.load(jsonfile)
 jsonfile.close()
 print(json_object)
-print(json_object["fields"]["summary"])
-print(json_object["fields"]["description"])
+# print(json_object["fields"]["summary"])
+# print(json_object["fields"]["description"])
 issueNo = ""
-for x in range(100):
+for x in range(5):
     issueNo = str(x)
     json_object["fields"]["summary"] = "Issue - " + issueNo
     json_object["fields"]["description"] = "Created using REST with python. This is description - "+ issueNo  
     json_string = json.dumps(json_object)
     response = requests.post(api_url, auth=('cpandey', 'administrator'),data=json_string, headers=headers)
     print("response of request: " + issueNo + response.text)
-print("Total issue created: "+issueNo);
+totalIssue = int(issueNo) + 1
+print("Total issue created: "+str(totalIssue));
 #with open(payload) as payloadfile:
 # response = requests.post(api_url, auth=('cpandey', 'administrator'),data=payloadfile, headers=headers)
 #print(response)
